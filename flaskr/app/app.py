62699fc59bbfee9175da492be67c2c2dabff91d0
Yash Rohatgi <rohatgiy@gmail.com>
2021-01-06 08:38:02 -0500

    deploying to heroku

--- flaskr/app/app.py
+++ flaskr/app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
-from youtube import main
 import youtube_dl
 import os
 
 app = Flask(__name__)
+
+MAX_LENGTH = 30 * 60
 
 if os.name == 'nt':
     import ctypes
@@ -58,7 +59,10 @@ ytdl_opts = {
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    'outtmpl': path
+    'outtmpl': path,
+    'noplaylist': True,
+    'playliststart': 1,
+    'playlistend': 1
 }
 
 @app.route('/', methods=['GET'])
@@ -71,7 +75,22 @@ def video():
         return redirect(url_for('index'))
     link = request.form['link']
     with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
-        ytdl.download([link]) 
-    return render_template('video.html', link=link)
+        try:
+            info = ytdl.extract_info(link, download=False)
+            if "_type" in info and info["_type"] == 'playlist':
+                return render_template('error.html', error="Cannot download playlist.")
+            else:
+                duration = info["duration"]
+            if  duration > MAX_LENGTH:
+                return render_template('error.html', error="Video duration exceeds 30 minutes.")
+            ytdl.download([link]) 
+            return render_template('video.html', title=info["title"], link=link)
+        except youtube_dl.utils.DownloadError as e:
+            error=str(e).lstrip('[0;31mERROR:[0m')
+            if error.find('is not a valid URL.') != -1:
+                error='Invalid URL.'
+            return render_template('error.html', error=error)
 
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect(url_for('index'))
